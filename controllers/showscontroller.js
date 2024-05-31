cb4938b85ad28ac7422f3da41cbe80c14b18e19d
yaacovkdev <yaacov.kochatkov@gmail.com>
2024-05-31 10:16:51 -0700

    modified query for frontend

--- controllers/showscontroller.js
+++ controllers/showscontroller.js
@@ -4,20 +4,25 @@ const list = async (req, res) => {
   const showname = req.query.name;
   try {
     let response;
-    if (showname) {
+    console.log(showname);
+    if(showname === "*") {
+      response = await db("tvshows").select("id", "name", "channel");
+    }
+    else if (showname) {
       response = await db("tvshows")
-        .where('name', 'like', `%${showname}%`)
-        .orWhere('original_name', 'like', `%${showname}%`);
-    } else {
-      response = await db("tvshows").select("*");
+        .select("id", "name", "channel")
+        .where("name", "like", `%${showname}%`)
+        .orWhere("original_name", "like", `%${showname}%`);
     }
     res.status(200).json(response);
-  } catch(error) {
+  } catch (error) {
     console.error(error);
-    res.status(500).json({Error: "An error occurred while retrieving the shows."});
+    res
+      .status(500)
+      .json({ Error: "An error occurred while retrieving the shows." });
   }
 };
 
 module.exports = {
   list,
-}
+};
